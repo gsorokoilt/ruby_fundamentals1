782f307aa884c62148505b3d4088222228070c16
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-19 17:21:05 -0400

    "create_greeting_based_on_name_length"

---
+++ exercise4.4.rb
@@ -0,0 +1,10 @@
+puts "Enter your full name"
+user_name=gets.chomp
+if user_name.length > 10
+  puts "Hello, #{user_name}"
+elsif user_name.length < 10
+  puts "Hi, #{user_name}"
+elsif user_name.length == 10
+  puts "Hey, #{user_name}"
+
+end
