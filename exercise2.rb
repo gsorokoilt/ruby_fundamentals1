4a06b64287a2a68cce36fbdaeacd904a03cbdbed
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-19 15:38:15 -0400

    "multiplication_using_string_interpolation"

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,3 @@
 #a good tip on $55
-puts "A good tip on a $55 meal is #{55*0.2} dollars"
+# puts "A good tip on a $55 meal is #{55*0.2} dollars"
+puts "45628 multiplied by 7839 equals #{45628*7839}"
