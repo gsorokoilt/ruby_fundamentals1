e83b8d53a4e0ca4d743fe900756f9a8a40130941
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-19 16:34:09 -0400

    "complete_assignment_4.1"

---
+++ exercise4.1.rb
@@ -0,0 +1,9 @@
+puts "Enter a number"
+number = gets.to_i
+if number > 100
+  puts "that's a big number!"
+elsif number< 100
+  puts "dream a little bigger"
+end
+
+# elsif number<=100 then puts "why not dream a little bigger"
