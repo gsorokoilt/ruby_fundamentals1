21e85ba5eef15eb42d1c2e49ef2cb7d1905f8e98
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-19 15:43:57 -0400

    "boolean_logic_equation"

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,7 @@
 #a good tip on $55
 # puts "A good tip on a $55 meal is #{55*0.2} dollars"
-puts "45628 multiplied by 7839 equals #{45628*7839}"
+# puts "45628 multiplied by 7839 equals #{45628*7839}"
+(10 < 20 && 30 < 20) || !(10 == 11) #is true
+(10 < 20 && 30 < 20) || !(false)
+(10 < 20 && 30 < 20) || true
+true
