ae4759636b1a998e96c48a22aa47c5a3d728462f
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-19 19:18:13 -0400

    "file_manipulation"

--- exercise5.2.rb
+++ exercise5.2.rb
@@ -1,18 +1,17 @@
 home = 0
 user_mode= ""
-energy=100
-puts "energy level is #{energy}"
 
-while home >=0 && user_mode != "go home" && energy >0
+
+while home >=0 && user_mode != "go home"
   puts "Would you like to walk or run?"
     user_mode=gets.chomp
 
     if user_mode == "walk"
     home+=1
-    energy+=1
+
     elsif user_mode == "run"
     home+=5
-    energy-=1
+
 else puts "Invalid response"
 
     end
